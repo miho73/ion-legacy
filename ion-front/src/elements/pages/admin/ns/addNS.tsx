83fc5405d70df5aac3f7d9a13fcf4061aeac854f
Yeji <hyrexfiels@gmail.com>
2023-07-12 19:00:52 +0900

    removed invalid test

---
+++ ion-front/src/elements/pages/admin/ns/addNS.tsx
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import { Alert, Button, Col, Form, InputGroup, Row } from "react-bootstrap";
+import { changeBit, getBit } from "../../../service/bitmask";
+import axios from "axios";
+import { inRange } from "../../../service/checker";
+
+function AddNs(props) {
+    const [revTime, setRevTime] = useState(-1);
+    const [revPlace, setRevPlace] = useState('');
+    const [revRes, setRevRes] = useState('');
+    const [formState, setFormState] = useState(0);
+
+    const [working, setWorking] = useState(false);
+    const [workError, setWorkError] = useState(-1);
+
+    const scode = props.scode;
+    const setScode = props.setScode;
+
+    function exe() {
+        let state = 0
+
+        // validate
+        if(revTime < 0 || revTime > 2) state = changeBit(state, 0);
+        if(!inRange(1, 30, revPlace.length)) state = changeBit(state, 1);
+        if(!inRange(1, 30, revRes.length)) state = changeBit(state, 3);
+
+        setFormState(state);
+
+        if(state != 0) return;
+
+        let time;
+        switch(revTime) {
+            case 0:
+                time = 'N8';
+                break;
+            case 1:
+                time = 'N1';
+                break;
+            case 2:
+                time = 'N2';
+                break;
+        }
+
+        axios.post('/manage/api/ns/create', {
+            scode: scode,
+            time: time,
+            place: revPlace,
+            reason: revRes
+        })
+        .then(res => {
+            setWorkError(0);
+        })
+        .catch(err => {
+            const es = err.response.data['result'];
+            switch(es) {
+                case 1:
+                    setWorkError(1);
+                    break;
+                case 2:
+                    setWorkError(2);
+                    break;
+                case 3:
+                    setWorkError(3);
+                    break;
+                case 4:
+                    setWorkError(4);
+                    break;
+                default:
+                    setWorkError(6);
+            }
+        });
+    }
+
+    return (
+        <Row className="my-3">
+            <h2 className="mb-3">면불 추가</h2>
+            <Form>
+                <Row className='mt-1'>
+                    <Form.Group as={Col} className='mb-3'>
+                        <Form.Label htmlFor='time' className='form-label'>면학</Form.Label>
+                        <Form.Select isInvalid={getBit(formState, 0) === 1} aria-label='면학 시간' disabled={working} value={revTime} onChange={e => setRevTime(Number.parseInt(e.target.value))}>
+                            <option value={-1}>면학 시간</option>
+                            <option value={0}>8면</option>
+                            <option value={1}>1면</option>
+                            <option value={2}>2면</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group as={Col} className='mb-3'>
+                        <Form.Label htmlFor='place' className='form-label'>장소</Form.Label>
+                        <Form.Control
+                            type='text'
+                            isInvalid={getBit(formState, 1) === 1}
+                            value={revPlace}
+                            disabled={working}
+                            onChange={e => setRevPlace(e.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} className='mb-3'>
+                        <Form.Label htmlFor='reason' className='form-label'>신청사유</Form.Label>
+                        <Form.Control
+                            type='text'
+                            isInvalid={getBit(formState, 3) === 1}
+                            value={revRes}
+                            disabled={working}
+                            onChange={e => setRevRes(e.target.value)}
+                        />
+                    </Form.Group>
+                </Row>
+                <Row>
+                    <Form.Group as={Col} className="mb-3">
+                        <InputGroup className='w-25'>
+                            <InputGroup.Text>학번</InputGroup.Text>
+                            <Form.Control
+                                type='number'
+                                placeholder='학번'
+                                value={scode}
+                                onChange={e => setScode(Number.parseInt(e.target.value))}
+                            />
+                            <Button onClick={exe}>등록</Button>
+                        </InputGroup>
+                    </Form.Group>
+                    {workError === 0 &&
+                        <Alert variant="success">면불을 추가했습니다.</Alert>
+                    }
+                    {workError === 1 &&
+                        <Alert variant="danger">작업을 하기 위한 권한이 부족합니다.</Alert>
+                    }
+                    {workError === 2 &&
+                        <Alert variant="danger">요청이 올바르지 않습니다.</Alert>
+                    }
+                    {workError === 3 &&
+                        <Alert variant="danger">IonID를 찾지 못했습니다.</Alert>
+                    }
+                    {workError === 4 &&
+                        <Alert variant="danger">이미 해당 시간에 신청된 면불이 있습니다.</Alert>
+                    }
+                    {workError === 6 &&
+                        <Alert variant="danger">면불을 추가하지 못했습니다.</Alert>
+                    }
+                </Row>
+            </Form>
+        </Row>
+    )
+}
+
+export default AddNs;
---
+++ ion-front/src/elements/pages/admin/ns/queryNs.tsx
@@ -0,0 +1,176 @@
+import React, { useState } from "react";
+import { Alert, Button, Col, Form, InputGroup, Modal, Row, Table } from "react-bootstrap";
+import NsState from "../../ns/nsState";
+import axios from "axios";
+
+function QueryNs(props) {
+    const [workError, setWorkError] = useState(-1);
+    const [nsLst, setNsLst] = useState<any[]>([]);
+
+    const [targetNs, setTargetNs] = useState([]);
+    const [deleteModalShow, setDeleteModalShow] = useState(false);
+    const [deleting, setDeleting] = useState(false);
+    const [deleteResult, setDeleteResult] = useState(-1);
+
+    const scode = props.scode;
+    const setScode = props.setScode;
+    
+    function exe() {
+        axios.get('/manage/api/ns/get-user', {
+            params: {code: scode}
+        })
+        .then(res => {
+            setNsLst(res.data['result']['reqs']);
+            setWorkError(0);
+        })
+        .catch(err => {
+            const cd = err.response.data['result'];
+            switch(cd) {
+                case 1:
+                    setWorkError(1);
+                    break;
+                case 2:
+                    setWorkError(2);
+                    break;
+                default:
+                    setWorkError(3);
+                    break;
+            }
+        });
+    }
+
+    let rr: any[] = [];
+    if(!workError) {
+        if(nsLst.length === 0) {
+            rr.push(
+                <tr>
+                    <td colSpan={7}>
+                        <p className='my-2 fw-bold'>신청한 면불이 없습니다.</p>
+                    </td>
+                </tr>
+            );
+        }
+        else {
+            nsLst.forEach(req => {
+                let row = <NsState
+                            name={req.time}
+                            place={req.place}
+                            superviser={req.supervisor}
+                            reason={req.reason}
+                            seat={req.lnsSeat}
+                            lnsReq={req.lnsReq}
+                            status={req.status}
+                            showDeleteConfirm={() => setDeleteModalShow(true)}
+                            setTargetNs={setTargetNs}
+                        />
+                rr.push(row);
+            });
+        }
+    }
+    else {
+        rr.push(
+            <tr>
+                <td className='table-danger' colSpan={7}>면불 신청 리스트를 받지 못했습니다.</td>
+            </tr>
+        )
+    }
+
+    function deleteNs() {
+        axios.delete('/manage/api/ns/delete', {
+            params: {
+                code: scode,
+                time: targetNs[0]
+            }
+        })
+        .then(res => {
+            closeDeleteConfirm();
+            exe();
+        })
+        .catch(err => {
+            setDeleteResult(1);
+        });
+    }
+    function closeDeleteConfirm() {
+        setDeleteModalShow(false);
+        setDeleteResult(-1);
+    }
+
+    return (
+        <>
+        <Row className='my-3'>
+            <h2 className="mb-3">면불 확인</h2>
+            <Form.Group as={Col}>
+                <InputGroup className='w-25 mb-3'>
+                    <InputGroup.Text>학번</InputGroup.Text>
+                    <Form.Control
+                        type='number'
+                        placeholder='학번'
+                        value={scode}
+                        onChange={e => setScode(Number.parseInt(e.target.value))}
+                    />
+                    <Button onClick={exe}>확인</Button>
+                </InputGroup>
+                { workError === 1 &&
+                    <Alert variant='danger w-fit'>작업에 필요한 권한이 없습니다.</Alert>
+                }
+                { workError === 2 &&
+                    <Alert variant='danger w-fit'>해당 IonID가 없습니다.</Alert>
+                }
+                { workError === 3 &&
+                    <Alert variant='danger w-fit'>작업을 처리하지 못했습니다.</Alert>
+                }
+                { workError === 0 &&
+                    <Table className='m-auto'>
+                        <thead>
+                            <tr>
+                                <th>면학</th>
+                                <th>장소</th>
+                                <th>담당교사</th>
+                                <th>사유</th>
+                                <th>노면실 자리</th>
+                                <th>상태</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>{rr}</tbody>
+                    </Table>
+                }
+            </Form.Group>
+        </Row>
+        <Modal show={deleteModalShow} onHide={closeDeleteConfirm} dialogClassName='modal-dialog-centered'>
+                <Modal.Header closeButton>
+                    <Modal.Title>면불 신청 삭제</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {deleteResult === -1 &&
+                        <p>{scode}의 {targetNs[1]}에 신청된 면불 신청을 삭제할까요?</p>
+                    }
+                    {deleteResult === 1 &&
+                        <p className='bg-danger text-white p-3 rounded'>삭제하지 못했습니다.</p>
+                    }
+                </Modal.Body>
+                <Modal.Footer>
+                    {deleteResult === -1 &&
+                        <>
+                            <Button onClick={deleteNs} disabled={deleting}>예</Button>
+                            <Button onClick={closeDeleteConfirm} disabled={deleting}>아니오</Button>
+                        </>
+                    }
+                    {deleteResult === 0 &&
+                        <>
+                            <Button onClick={closeDeleteConfirm}>확인</Button>
+                        </>
+                    }
+                    {deleteResult === 1 &&
+                        <>
+                            <Button onClick={deleteNs} disabled={deleting}>재시도</Button>
+                            <Button onClick={closeDeleteConfirm} disabled={deleting}>취소</Button>
+                        </>
+                    }
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default QueryNs;
